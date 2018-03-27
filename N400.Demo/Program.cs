deb3b96bfe282f9c8fe123ef5fbfbf8959712728
Calvin <calvin@cmpct.info>
2018-03-27 20:24:28 -0300

    clean up example
    it's not a very good one, more a testbed I use (conside rthat a TODO)

--- N400.Demo/Program.cs
+++ N400.Demo/Program.cs
@@ -10,15 +10,15 @@ namespace N400.Demo
     {
         static void Main(string[] args)
         {
+            Console.Write("Hostname: ");
+            var hostname = Console.ReadLine();
+            Console.Write("Username: ");
+            var username = Console.ReadLine();
             Console.Write("Password: ");
             var password = Console.ReadLine();
 
-            var s = new Server("173.95.177.177", "calvin", password, true, portMapperMode: PortMapperMode.AlwaysUsePortMapper);
-            //var s = new Server("173.54.20.170", "calvin", password, false, portMapperMode: PortMapperMode.AlwaysUsePortMapper);
+            var s = new Server(hostname, username, password, portMapperMode: PortMapperMode.AlwaysUsePortMapper);
             s.Signon();
-
-            //var remote = new N400.Services.RemoteCommandService(s);
-            //remote.GetJobInfo();
 
             var dq = new DataQueues.DataQueue(s, "CALVIN", "TESTQ");
             dq.Create(8);
